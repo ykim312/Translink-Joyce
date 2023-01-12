c11d385ad6721d1f6548ad07d8aa4419b16a2a99
ykim312 <ykim312@my.bcit.ca>
2023-01-11 22:29:02 -0800

    Adding heading and header

---
+++ components/Map/footer.js
@@ -0,0 +1,11 @@
+import style from "../../styles/Home.module.css";
+
+
+export default function Footer(){
+    return (
+        <div className={style.footer}>
+            <p>Waterfront Station to King George Station</p>
+            <p>Waterfront Station to Production Way–University Station</p>
+        </div>
+    )
+}
---
+++ components/Map/header.js
@@ -0,0 +1,10 @@
+import style from "../../styles/Home.module.css";
+
+
+export default function Header(){
+    return (
+        <div className={style.header}>
+            <h1>Translink Skytrain</h1>
+        </div>
+    )
+}
